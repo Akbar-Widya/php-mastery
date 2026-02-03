6ce0233aaaa3d19c1770aef4e89362ecee216fd0
Akbar Widya P. <akbarwidya@student.telkomuniversity.ac.id>
2026-02-03 19:53:32 +0700

    Track 1: Programming Syntax. Topic 2: Basic Operations, Sub: Math and Text Merging

--- index.php
+++ index.php
@@ -1,27 +1,20 @@
 <?php
 
-declare(stric_types=1);
+declare(strict_types=1);
 
-/**
- * Technical Increment: Primitives
- * No Classes, No Interfaces. Just data.
-*/
+// 1. Numerical Operations
+$basePrice = 100;
+$taxAmount = 15;
+$totalPrice = $basePrice + $taxAmount; // Addition
 
-// 1. String: text
-$tool = "PHP 8.4";
+$discount = 10;
+$finalPrice = $totalPrice - $discount; // Subtraction
 
-// 2. Integer: whole number
-$quantity = 5;
+// 2. String Operations; The Dot Operator
+// In PHP, we use a period (.) to glue text together.
+$greeting = "Total Amount Due: ";
+$message = $greeting . "$" . $finalPrice; 
 
-// 3. Float: decimal
-$price = 19.99;
+echo $message;
 
-// 4. Boolean: true or false
-$isReady = true;
-
-// 5. Basic Math
-$total = $quantity * $price;
-
-echo "Learning " . $tool . "\n";
-echo "Total Price: " . $total;
 ?>
